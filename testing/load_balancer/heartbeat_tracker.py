cb35ba52a5ccd3e37901afc37e971eaccafc1f65
Mikko Takala <mikkotakala@hotmail.com>
2021-12-05 21:23:08 +0200

    Added thread safety to load balancer heartbeat functionality. Removed unused variable

--- testing/load_balancer/heartbeat_tracker.py
+++ testing/load_balancer/heartbeat_tracker.py
@@ -1,19 +1,20 @@
+from threading import Lock
 import time
 
 class Heartbeat:
-  TIMEOUT_LIMIT = 10
   server_heartbeats = {}
+  heartbeats_lock = Lock()
 
   def check_timeouts_against_interval(self, interval):
     print('Checking for expired heartbeats...')
-    # Should lock hb dict
     timeouted_servers = []
 
+    self.heartbeats_lock.acquire()
     for server in self.server_heartbeats.keys():
       if time.time() - self.server_heartbeats[server] >= interval:
         timeouted_servers.append(server)
 
-    # Should unlock hb dict
+    self.heartbeats_lock.release()
     return timeouted_servers
 
   def watch_timeouts(self, interval, callback):
@@ -26,17 +27,17 @@ class Heartbeat:
         self.remove_heartbeats_from_dict(timeouted_servers)
         callback(timeouted_servers)
       
-      time.sleep(30)
+      time.sleep(interval)
 
   def remove_heartbeats_from_dict(self, heartbeats_to_remove):
     print('Removing expired heartbeats from dictionary...')
-    # Should lock hb dict
+    self.heartbeats_lock.acquire()
     for heartbeat in heartbeats_to_remove:
       self.server_heartbeats.pop(heartbeat, None)
-    # Should unlock hb dict
+    self.heartbeats_lock.release()
 
   def receive_heartbeat(self, server_id):
-    # Should lock hb dict
+    self.heartbeats_lock.acquire()
     print(f'Heartbeat received from {server_id}')
     self.server_heartbeats[server_id] = time.time()
-    # Should unlock hb dict
+    self.heartbeats_lock.release()
